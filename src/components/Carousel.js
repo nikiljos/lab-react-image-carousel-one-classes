dab0b824d07ab1b4fbc4580c084045fdfad4a9f0
Nikhil Jose <nikhiljose12345@gmail.com>
2022-12-24 11:44:28 +0530

    implement carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,7 +7,44 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
-  
+    constructor(){
+        super();
+        this.state={
+            currentSlide:0,
+        }
+    }
+    
+    changeImage(isForward){
+        this.setState((prevState)=>{
+            let next=prevState.currentSlide;
+            next=isForward?next+1:next-1;
+            if(next<0){
+                next=0;
+            }
+            else if(next>2){
+                next=2;
+            }
+            return {
+                currentSlide: next,
+            };
+        })
+    }
+
+    render(){
+        let data=images[this.state.currentSlide]
+        return (
+            <div className="Carousel">
+                <button onClick={() => this.changeImage(false)} className="back"> <ArrowBackIosIcon sx={{ color:"#ffffff",fontSize: 50, padding:"10px" }}/> </button>
+                <button onClick={() => this.changeImage(true)} className="forward"> <ArrowForwardIosIcon sx={{ color:"#ffffff",fontSize: 50, padding:"10px" }}/> </button>
+
+                <div className="content">
+                    <h1 className="overlay">{data.title}</h1>
+                    <h3 className="overlay">{data.subtitle}</h3>
+                    <img src={data.img} alt="" />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Carousel;
